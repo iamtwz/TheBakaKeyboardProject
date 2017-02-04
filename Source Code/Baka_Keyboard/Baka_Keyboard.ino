966670899b65c3b609dd406a4265d598bc77d336
iamtwz <i@twznow.com>
2017-02-04 09:43:05 +0800

    update

---
+++ Source Code/Baka_Keyboard/Baka_Keyboard.ino
@@ -0,0 +1,28 @@
+// use this option for OSX:
+char ctrlKey = KEY_LEFT_GUI;
+// use this option for Windows and Linux:
+//  char ctrlKey = KEY_LEFT_CTRL;  
+
+void setup() {
+  // make pin 2 an input and turn on the 
+  // pullup resistor so it goes high unless
+  // connected to ground:
+  pinMode(2, INPUT_PULLUP);
+  // initialize control over the keyboard:
+  Keyboard.begin();
+}
+
+void loop() {
+  while (digitalRead(2) == HIGH) {
+    // do nothing until pin 2 goes low
+    delay(500);
+  }
+  delay(1000);
+  // new document:
+  Keyboard.press(ctrlKey);
+  Keyboard.press('n');
+  delay(100);
+  Keyboard.releaseAll();
+  // wait for new window to open:
+  delay(1000);
+}
